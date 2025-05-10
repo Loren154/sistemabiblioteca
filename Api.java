340946a730b341b7e2d39472b8186ed9362c9829
¨Loren154¨ <tajiboywendy0@gmail.comgit pull origin maingit config --global user.name loren154git config --global user.email tajiboywendy0@gmail.com>
2025-05-10 15:02:50 -0600

    primer commit

---
+++ Api.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.api;
+
+/**
+ *
+ * @author ANITA
+ */
+public class Api {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
